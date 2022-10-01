17092c0cd05a921c1daf2e14982d76cb7176406b
Gabriel Chan <gaibryol@gmail.com>
2022-09-30 18:47:02 -0600

    Added game manager and constants

---
+++ Assets/GameAssets/Code/Constants.cs
@@ -0,0 +1,4 @@
+public static class Constants
+{
+    public enum GameStates { MainMenu = 0, Settings = 1, Game = 2, Pause = 3 }
+}
---
+++ Assets/GameAssets/Code/GameManager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+	public static GameManager Instance { get; private set; }
+
+	public Constants.GameStates GameState;
+
+	private void Awake()
+	{
+		if (Instance != null && Instance != this)
+		{
+			Destroy(this.gameObject);
+		}
+		else
+		{
+			Instance = this;
+		}
+
+		GameState = Constants.GameStates.MainMenu;
+	}
+}
