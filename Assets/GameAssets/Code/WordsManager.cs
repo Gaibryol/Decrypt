42d8036dbdad5e6733bdf7971b6a8c2c76bc2b98
BhanHG <96505078+BhanHG@users.noreply.github.com>
2022-09-30 22:37:37 -0600

    Added WordsManager
    Ability to get random scrambled words based on possible words lengths. You can changed the possible word lengths and check if the word you unscrambled is correct.

---
+++ Assets/GameAssets/Code/WordsManager.cs
@@ -0,0 +1,133 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class WordsManager : MonoBehaviour
+{
+    public static WordsManager Instance;
+    private string possibleWordLengths = "4,5,6";
+    private List<int> odds;
+    private List<string> spawnedList = new List<string>();
+    private void Awake()
+    {
+        if(Instance = null){
+            Instance = this;
+        }
+    }
+    private void Start() 
+    {
+    }
+    private int GetNumOfLetters()
+    {
+        switch(possibleWordLengths){
+            case Constants.FourFiveSix:
+                odds = new List<int>(){4,4,4,5,5,5,5,5,5,5,6,6,6,6};
+                break;
+            case Constants.ThreeFourFiveSix:
+                odds = new List<int>(){3,3,4,4,4,5,5,5,5,5,5,5,6,6,6,6};
+                break;
+            case Constants.ThreeFourFiveSixSeven:
+                odds = new List<int>(){3,3,3,3,4,4,4,4,4,4,5,5,5,5,5,5,5,5,5,5,5,6,6,6,6,6,6,6,7,7,7};
+                break;
+            case Constants.ThreeFourFiveSixSevenEight:
+                odds = new List<int>(){3,3,3,4,4,4,4,4,4,5,5,5,5,5,5,5,5,5,5,5,6,6,6,6,6,6,6,7,7,7,7,8,8};
+                break;
+            case Constants.FourFiveSixSeven:
+                odds = new List<int>(){4,4,4,5,5,5,5,5,5,5,6,6,6,6,7,7};
+                break;
+            case Constants.FourFiveSixSevenEight:
+                odds = new List<int>(){4,4,4,4,5,5,5,5,5,5,5,5,6,6,6,6,6,7,7,7,8,8};
+                break;
+            case Constants.FourFiveSixEight:
+                odds = new List<int>(){4,4,4,4,5,5,5,5,5,5,5,5,6,6,6,6,6,8,8};
+                break;
+            case Constants.ThreeFourFiveSixEight:
+                odds = new List<int>(){3,3,3,4,4,4,4,5,5,5,5,5,5,5,5,6,6,6,6,6,8,8};
+                break;
+        }
+        int randomNum = Random.Range(0,odds.Count);
+        int numLetters = odds[randomNum];
+        return numLetters;
+
+
+    }
+    private string GetRandomWord()
+    {
+        int numLetters = GetNumOfLetters();
+        List<string> aList;
+        switch(numLetters){
+            case 3:
+                aList = Constants.ThreeLetterList;
+                break;
+            case 4:
+                aList = Constants.FourLetterList;
+                break;
+            case 5:
+                aList = Constants.FiveLetterList;
+                break;
+            case 6:
+                aList = Constants.SixLetterList;
+                break;
+            case 7:
+                aList = Constants.SevenLetterList;
+                break;
+            default:
+                aList = Constants.EightLetterList;
+                break;
+        }
+        int randomNum = Random.Range(0,aList.Count);
+        string randomWord = aList[randomNum];
+        while (spawnedList.Contains(randomWord))
+        {
+            randomNum = Random.Range(0,aList.Count);
+            randomWord = aList[randomNum];
+        }
+        spawnedList.Add(randomWord);
+        return randomWord;
+    }
+
+    private string Shuffle(string word)
+    {
+        char[] characters= word.ToCharArray();
+        char tempCharacter;
+        for(int i = 0; i < characters.Length - 1; i++)
+        {
+            int randomNum = Random.Range(i,characters.Length);
+            tempCharacter = characters[randomNum];
+            characters[randomNum] = characters[i];
+            characters[i] = tempCharacter;
+        }
+        return new string(characters);
+    }
+    public string GetScrambledWord()
+    {
+        string randomWord = GetRandomWord();
+        string shuffledWord = Shuffle(randomWord);
+        while(IsCorrect(shuffledWord))
+        {
+            shuffledWord = Shuffle(randomWord);
+        }
+        return shuffledWord;
+    }
+    public void ChangePossibleWordLength(string list){
+        possibleWordLengths = list;
+    }
+    public bool IsCorrect(string word)
+    {
+        
+        switch(word.Length)
+        {
+            case 3:
+                return Constants.ThreeLetterList.Contains(word);
+            case 4:
+                return Constants.FourLetterList.Contains(word);
+            case 5:
+                return Constants.FiveLetterList.Contains(word);
+            case 6:
+                return Constants.SixLetterList.Contains(word);
+            case 7:
+                return Constants.SevenLetterList.Contains(word);
+            default:
+                return Constants.EightLetterList.Contains(word);
+        }
+    }
+}
